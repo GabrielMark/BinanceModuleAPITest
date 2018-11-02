cb570470bc56d6f7dec416e607bbe8419807fd0a
Gabriel Sampson <GabrielmSampson@gmail.com>
2018-11-02 16:20:59 -0600

    Added function to find price of Ethereum and log price.

---
+++ index.js
@@ -0,0 +1,14 @@
+const binance = require('node-binance-api')().options({
+  APIKEY: '<key>',
+  APISECRET: '<secret>',
+  useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
+  test: true // If you want to use sandbox mode where orders are simulated
+});
+
+function findPriceETH(){
+  binance.prices('ETHBTC', (error, ticker) => {
+    console.log("The price of Ethereum is: " + ticker.ETHBTC);
+  });
+};
+
+findPriceETH();
